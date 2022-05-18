da307510cf0d993e461fa7eb08076c7d22c388be
Alicia Padrón <aliciaapadron@gmail.com>
2022-05-18 11:18:57 +0200

    Configure the database in Node JS& try SELECT

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const moviesData = require('./data/movies.json');
 const users = require('./data/users.json');
+const Database = require('better-sqlite3');
 
 // create and config server
 const server = express();
@@ -27,28 +28,6 @@ server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
-server.get('/movies', (req, res) => {
-  //guardamos el valor del query en una constante
-  const genderFilterParam = req.query.gender ? req.query.gender : '';
-  //aquí respondemos con el listado filtrado
-  res.json({
-    success: true,
-    movies: moviesData
-      .filter((item) => item.gender.includes(genderFilterParam))
-      //función para ordenar
-      //"asc" hace referencia al value del input A-Z en AllMovies.js
-      //Se compara con -1 porque en la segunda condición le estamos indicando que la cadena z o referenceStr(z-a) iría por delante de a o compareString(a-z)
-      .sort(function (a, z) {
-        const sortFilterParam = a.title.localeCompare(z.title);
-        if (req.query.sort === 'asc') {
-          return sortFilterParam;
-        } else {
-          return sortFilterParam * -1;
-        }
-      }),
-  });
-});
-
 server.post('/login', (req, res) => {
   let exist = users.find((user) => {
     if (user.email === req.body.email && user.password === req.body.password) {
@@ -70,13 +49,61 @@ server.post('/login', (req, res) => {
   });
 });
 
-server.get('/movie/:movieId', (req, res) => {
+//DEJO COMENTADOS EL GET MOVIES Y EL GET MOVIES PORQUE ABAJO HE ESTADO PROBADO A HACER LO DEL SELECT
+
+/*server.get('/movies', (req, res) => {
+  //guardamos el valor del query en una constante
+  const genderFilterParam = req.query.gender ? req.query.gender : '';
+  //aquí respondemos con el listado filtrado
+  res.json({
+    success: true,
+    movies: moviesData
+      .filter((item) => item.gender.includes(genderFilterParam))
+      //función para ordenar
+      //"asc" hace referencia al value del input A-Z en AllMovies.js
+      //Se compara con -1 porque en la segunda condición le estamos indicando que la cadena z o referenceStr(z-a) iría por delante de a o compareString(a-z)
+      .sort(function (a, z) {
+        const sortFilterParam = a.title.localeCompare(z.title);
+        if (req.query.sort === 'asc') {
+          return sortFilterParam;
+        } else {
+          return sortFilterParam * -1;
+        }
+      }),
+  });
+}); */
+
+/*server.get('/movie/:movieId', (req, res) => {
   // console.log('URL params:', req.params);
   // console.log('URL params id:', req.params.movieId);
   const foundMovie = moviesData.find((movie) => {
     return movie.id === req.params.movieId;
   });
   console.log('La peli es:', foundMovie);
+  if (foundMovie) {
+    res.render('movie', foundMovie);
+  } else {
+    const error = { error: req.url };
+    res.render('movie-not-found', error);
+  }
+});*/
+
+//2.Configura la base de datos en NODE JS
+const db = Database('./src/data/database.db', { verbose: console.log });
+
+//3.Haz un SELECT para obtener todas las películas
+
+server.get('/movies', (req, res) => {
+  const query = db.prepare(`SELECT  * FROM movies  ORDER BY  name `);
+  const moviesList = query.all();
+  res.render('movie', { moviesList });
+});
+
+server.get('/movie/:movieId', (req, res) => {
+  const query = db.prepare(`SELECT  * FROM movies WHERE  name = ? `);
+  const foundMovie = moviesList.find((movie) => {
+    return movie.id === req.params.movieId;
+  });
   if (foundMovie) {
     res.render('movie', foundMovie);
   } else {
